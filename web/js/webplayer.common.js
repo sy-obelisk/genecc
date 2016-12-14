b09c05d88107f81ec08ba0c3fa4e41f1ba7cea65
Anne <anne.z@thinkwithu.com>
2016-12-14 16:04:59 +0800

    添加打赏弹窗

--- web/js/webplayer.common.js
+++ web/js/webplayer.common.js
@@ -17,7 +17,8 @@ $(function () {
     channel.bind("onPlayheadTime", function (event) {
         var width=event.data.playheadTime/videoduration*100;
         if(width>99.8){
-            
+            $(".form-maskLayer").show();
+            $(".form-font").show();
         }
         $(".progress-bar-buffer").css("width", event.data.downloadProgress + "%");//下载百分比
         $("#playerProgressBar").slider('value', width);//滚动条
