cd4d5d17abf300da713f1843a52d7fcaf4b5ce47
Anne <anne.z@thinkwithu.com>
2016-12-20 15:47:14 +0800

    ios页面

--- views/index/ios.php
+++ views/index/ios.php
@@ -25,7 +25,7 @@
     <!-- !!!注意 minimal-ui 是IOS7.1的新属性，最小化浏览器UI -->
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <meta name="format-detection" content="telephone=no">
-    <link rel="stylesheet" href="/css/new-live.css"/>
+    <link rel="stylesheet" href="/css/ios-live.css"/>
     <link rel="stylesheet" href="/css/jquery.nstSlider.css"/>
     <script type="text/javascript" src="/js/jquery-1.9.1.min.js"></script>
     <script type="text/javascript" src="/js/jquery.nstSlider.js"></script>
@@ -79,21 +79,22 @@
 <!--    </div>-->
 </div>
 <!--底部黑色条部分-->
-<!--<div class="black_control">
+<div class="black_control">
     <div class="play" onclick="playIcon(this)"></div>
     <div class="volume">
-        <div class="volume_icon">
-            <img src="/images/video_volume.png" alt="音量图标">
-        </div>
-        <div class="volume_pro">
-            <div class="nstSlider" data-range_min="0" data-range_max="100"
-                 data-cur_min="30"  data-cur_max="0">
-                <div class="bar bot"></div>
-                <div class="leftGrip bot"></div>
-            </div>
-        </div>
+<!--        <div class="volume_icon">-->
+<!--            <img src="/images/video_volume.png" alt="音量图标">-->
+<!--        </div>-->
+<!--        <div class="volume_pro">-->
+<!--            <div class="nstSlider" data-range_min="0" data-range_max="100"-->
+<!--                 data-cur_min="30"  data-cur_max="0">-->
+<!--                <div class="bar bot"></div>-->
+<!--                <div class="leftGrip bot"></div>-->
+<!--            </div>-->
+<!---->
+<!--        </div>-->
     </div>
-</div>-->
+</div>
 <div class="center_line"></div>
 <!--老师视频部分-->
 <div class="teacher_box">
