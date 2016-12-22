0cddbf059856e48f85155f229cc70891bc45d0be
Obelisk <352627129@qq.com>
2016-12-22 17:01:56 +0800

    test

--- views/index/androidApp.php
+++ views/index/androidApp.php
@@ -25,7 +25,7 @@
     <!-- !!!注意 minimal-ui 是IOS7.1的新属性，最小化浏览器UI -->
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <meta name="format-detection" content="telephone=no">
-    <link rel="stylesheet" href="/css/new-live.css"/>
+    <link rel="stylesheet" href="/css/ios-live.css"/>
     <link rel="stylesheet" href="/css/jquery.nstSlider.css"/>
     <script type="text/javascript" src="/js/jquery-1.9.1.min.js"></script>
     <script type="text/javascript" src="/js/jquery.nstSlider.js"></script>
@@ -48,51 +48,59 @@
 <body>
 <!--标题栏-->
 <div class="blackNav">
+    <a href="/index" class="returnP"><img src="/images/videoL_lj.png" alt="白色返回箭头"/></a>
     <span><?php echo $name?></span>
 </div>
 <div class="play_main">
     <!--放ppt的容器-->
-     <div class="play_con">
-         <gs:video-vod
-             site="bjsy.gensee.com"
-             ctx="training"
-             ownerid="<?php echo $sdk?>"
-             uid="14889"
-             uname="2"
-             password=""
-             authcode=""
-             encodetype=""
-             bgimg="http://www.gmatonline.cn/app/web_core/styles//images/bg-video.png"
-             bar="false"
-             py="1"
-             lang="zh_CN"/>
-     </div>
+    <div class="play_con">
+        <gs:video-vod
+            site="bjsy.gensee.com"
+            ctx="training"
+            ownerid="<?php echo $sdk?>"
+            uid="14889"
+            uname="2"
+            password=""
+            authcode=""
+            encodetype=""
+            bgimg="http://www.gmatonline.cn/app/web_core/styles//images/bg-video.png"
+            bar="false"
+            py="1"
+            lang="zh_CN"/>
+    </div>
     <!--进度条-->
-<!--    <div class="my_pro">-->
-<!--        <div class="nstSlider top" data-range_min="0" data-range_max="100"-->
-<!--             data-cur_min="30"  data-cur_max="0">-->
-<!--            <div class="bar top"></div>-->
-<!--            <div class="leftGrip top"></div>-->
-<!--        </div>-->
-<!--        <div class="rightLabel">01:32:28</div>-->
-<!--    </div>-->
+    <!--    <div class="my_pro">-->
+    <!--        <div class="nstSlider top" data-range_min="0" data-range_max="100"-->
+    <!--             data-cur_min="30"  data-cur_max="0">-->
+    <!--            <div class="bar top"></div>-->
+    <!--            <div class="leftGrip top"></div>-->
+    <!--        </div>-->
+    <!--        <div class="rightLabel">01:32:28</div>-->
+    <!--    </div>-->
 </div>
 <!--底部黑色条部分-->
-<!--<div class="black_control">
-    <div class="play" onclick="playIcon(this)"></div>
+<div class="black_control">
+    <div id="iosPlay" class="play"></div>
     <div class="volume">
-        <div class="volume_icon">
-            <img src="/images/video_volume.png" alt="音量图标">
-        </div>
-        <div class="volume_pro">
-            <div class="nstSlider" data-range_min="0" data-range_max="100"
-                 data-cur_min="30"  data-cur_max="0">
-                <div class="bar bot"></div>
-                <div class="leftGrip bot"></div>
+        <!--        <div class="volume_icon">-->
+        <!--            <img src="/images/video_volume.png" alt="音量图标">-->
+        <!--        </div>-->
+        <!--        <div class="volume_pro">-->
+        <!--            <div class="nstSlider" data-range_min="0" data-range_max="100"-->
+        <!--                 data-cur_min="30"  data-cur_max="0">-->
+        <!--                <div class="bar bot"></div>-->
+        <!--                <div class="leftGrip bot"></div>-->
+        <!--            </div>-->
+        <!---->
+        <!--        </div>-->
+        <div class="player-progress-bar-wrap pro_bg01">
+            <div class="player-slider progress-bar-elapsed pro_bg02"></div>
+            <div class="player-slider progress-bar-buffer pro_load"></div>
+            <div id="playerProgressBar" class="player-progress-bar pro_bar">
             </div>
         </div>
     </div>
-</div>-->
+</div>
 <div class="center_line"></div>
 <!--老师视频部分-->
 <div class="teacher_box">
@@ -105,7 +113,6 @@
         lang="zh_CN"/>
 </div>
 <!--打赏弹窗-->
-
 
 <script type="text/javascript">
     $(function(){
